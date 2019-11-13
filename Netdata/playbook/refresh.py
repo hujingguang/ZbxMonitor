2aef5a4eadfa0206c11d5b0ae4798e367ce984de
hujingguang <hujingguangsa@gmail.com>
2019-11-13 17:02:49 +0800

    优化refresh.py

--- Netdata/playbook/refresh.py
+++ Netdata/playbook/refresh.py
@@ -2,6 +2,22 @@
 import os
 import socket
 import MySQLdb
+import ConfigParser
+import io
+
+
+HOSTS="""
+[bigdata]
+10.56.80.17
+10.56.80.11
+10.56.80.12
+10.56.80.13
+10.56.80.14
+10.56.80.15
+10.56.80.16
+[ops]
+10.57.0.31
+"""
 
 
 TEMPLATE_UPSTREAM = """
@@ -33,30 +49,46 @@ server {
     }
 }
 """
-NGINX_CONF_DIR = "/etc/nginx/conf.d/netdata"
-NGINX_SERVER_FILE = "/etc/nginx/conf.d/netdata.conf"
-HOSTS_FILE = "/etc/nginx/hosts"
-MYSQL_HOST='10.210.110.210'
+NGINX_CONF_DIR = "/usr/local/openresty/nginx/upstreams.d/netdata"
+NGINX_SERVER_FILE = "/usr/local/openresty/nginx/vhosts/archery.yxzq.com.conf"
+MYSQL_HOST='10.57.0.12'
 MYSQL_PORT=3306
-MYSQL_USER="ops"
+MYSQL_USER="db_archery"
 MYSQL_DB="archery"
-MYSQL_PASSWD="Ops$123$123"
+MYSQL_PASSWD="42"
 MYSQL_TABLE="sql_netdata_monitor"
 
 
 
-def sync_ip_info_to_db(ip,tport):
+
+
+
+def parser_config():
+    result = {}
+    config = ConfigParser.RawConfigParser(allow_no_value=True)
+    config.readfp(io.BytesIO(HOSTS))
+    groups = config.sections()
+    for group in groups:
+        result[group] = list()
+        for item in config.items(section=group):
+            result[group].append(item)
+    return result
+    
+def sync_ip_info_to_db(ip,tport,group,delete=False):
     host,port,user,db,passwd,table = MYSQL_HOST,MYSQL_PORT,MYSQL_USER,MYSQL_DB,MYSQL_PASSWD,MYSQL_TABLE
     try:
         conn = MySQLdb.connect(host=host,user=user,passwd=passwd,db=db,port=port,charset="utf8")
         cursor = conn.cursor()
-        where_sql = 'select * from '+table+' where listen_address ="'+ip+'";'
-        where_sql = 'select * from '+table+' where listen_address="'+ip+'";'
-        insert_sql = 'use '+db+' ;insert into '+table+' (listen_address,listen_port) values ("'+ip+'",'+str(tport)+');'
-        cursor.execute(where_sql)
-        result = cursor.fetchone()
-        if not result:
-            cursor.execute(insert_sql)
+        if delete:
+            delete_sql = "delete from "+table+" where listen_address='"+ip+"' and group_name = '"+group+"';"
+            cursor.execute(delete_sql)
+        else:
+            where_sql = 'select * from '+table+' where listen_address="'+ip+'" and group_name = "' +group+'";'
+            insert_sql = 'use '+db+' ;insert into '+table+' (listen_address,listen_port,group_name) values ("'+ip+'",'+str(tport)+',"'+group+'");'
+            cursor.execute(where_sql)
+            result = cursor.fetchone()
+            if not result:
+                cursor.execute(insert_sql)
     except Exception as e:
         print str(e)
     else:
@@ -80,35 +112,31 @@ def check_netdata_port(host,port):
         return True
     
 def reload_nginx():
-    os.system("/usr/sbin/nginx -t &>/dev/null && /usr/sbin/nginx -s reload")
+    os.system(" /usr/local/openresty/nginx/sbin/nginx -t &>/dev/null &&  /usr/local/openresty/nginx/sbin/nginx -s reload")
+
 def main():
-    hosts = HOSTS_FILE
     nginx_conf = NGINX_CONF_DIR
     template_upstream = TEMPLATE_UPSTREAM
-    ip_port_list = list()
-    if os.path.exists(hosts):
-        with open(hosts,"r") as f:
-            for line in f.readlines():
-                line = line.replace("\n","").split(":")
-                result = line if len(line) >1 else [line[0],19999]
-                ip_port_list.append(result)
-
+    result = parser_config()
     nginx_server_file = NGINX_SERVER_FILE
     if not os.path.exists(nginx_server_file):
         write_server_http_file(nginx_server_file)
-    for ip_port in ip_port_list:
-        ip,port = ip_port
-        if check_netdata_port(ip,port):
-            ip_path = os.path.join(nginx_conf,ip+".conf")
-            if not os.path.exists(ip_path):
-                sync_ip_info_to_db(ip,port)
-                with open(ip_path,"w") as f:
-                    f.write(template_upstream.replace("xxxxxx",ip))
+    for group,ip_port_list in result.iteritems():
+        for ip_port in ip_port_list:
+            ip,port = ip_port
+            port = 19999 if not port else int(port)
+            if check_netdata_port(ip,port):
+                ip_path = os.path.join(nginx_conf,ip+".conf")
+                if not os.path.exists(ip_path):
+                    sync_ip_info_to_db(ip,port,group)
+                    with open(ip_path,"w") as f:
+                        f.write(template_upstream.replace("xxxxxx",ip))
+            else:
+                sync_ip_info_to_db(ip,port,group,delete=True)
     reload_nginx()
 
 if __name__ == "__main__":
     main()
-            
 
 
 
